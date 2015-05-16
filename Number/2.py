8212d4e9980bc2d9b4b320b573fbd38e09a817c0
egoing <egoing@gmail.com>
2015-05-16 15:53:16 +0900

    좀 더 복잡한 계산

---
+++ Number/2.py
@@ -0,0 +1,5 @@
+import math
+print(math.ceil(2.2))
+print(math.floor(2.7))
+print(math.pow(2,10))
+print(math.pi)
---
+++ Number/2.rb
@@ -0,0 +1,5 @@
+
+puts( 2.2.ceil() )
+puts( 2.7.floor() )
+puts( 2**10 )
+puts( Math::PI )
