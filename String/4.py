1b58d8cb03ff308cddd9b3718323154be5a9552e
egoing <egoing@gmail.com>
2015-05-20 16:27:33 +0900

    특수문자와 데이터형 예제 추가

---
+++ String/4.py
@@ -0,0 +1,6 @@
+print("egoing's \"tutorial\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a")
+print('Hello\nworld')
---
+++ String/4.rb
@@ -0,0 +1,6 @@
+puts("egoing's \"tutorial\"")
+puts("\\")
+puts("Hello\nworld")
+puts("Hello\t\tworld")
+puts("\a")
+puts('Hello\nworld')
