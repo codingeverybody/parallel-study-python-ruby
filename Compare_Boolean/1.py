b2f28e1d388933da3d70a2450994db5ae9f117f4
egoing <egoing@gmail.com>
2015-05-31 02:08:48 +0900

    비교와 블리언 실습

---
+++ Compare_Boolean/1.py
@@ -0,0 +1,8 @@
+a=1
+b=1
+print(a==b)
+print(1==2)
+print(1>2)
+print(1<2)
+print(True)
+print(False)
---
+++ Compare_Boolean/1.rb
@@ -0,0 +1,8 @@
+a=1
+b=1
+puts(a==b)
+puts(1==2)
+puts(1>2)
+puts(1<2)
+puts(true)
+puts(false)
