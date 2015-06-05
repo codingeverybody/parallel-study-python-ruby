030eb4d7dae9dd07557f5123d98bf560e2da1827
egoing <egoing@gmail.com>
2015-06-05 23:24:27 +0900

    Comment

---
+++ Comment/1.py
@@ -0,0 +1,15 @@
+'''
+조건문 예제
+egoing
+2015
+'''
+# user input password
+input = 33
+real_egoing = 11
+#real_k8805 = "ab"
+if real_egoing == input:
+  print("Hello!, egoing")
+#elif real_k8805 == input:
+#  print("Hello!, k8805")
+else:
+  print("Who are you?")
---
+++ Comment/1.rb
@@ -0,0 +1,14 @@
+# 조건문 예제
+# egoing
+# 2015
+
+input = 33
+real_egoing = 11
+#real_k8805 = "ab"
+if real_egoing == input
+  puts("Hello!, egoing")
+#elsif real_k8805 == input
+#  puts("Hello!, k8805")
+else
+  puts("Who are you?")
+end
