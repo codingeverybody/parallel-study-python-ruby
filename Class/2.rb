c5303ae82a868d35ce6ee69d6f9c4a50cefa7d8f
egoing <egoing@gmail.com>
2015-07-11 17:40:12 +0900

    none oop style

---
+++ Class/2.rb
@@ -0,0 +1,16 @@
+def add(v1, v2)
+  return v1+v2
+end
+def subtract(v1, v2)
+  return v1-v2
+end
+
+num1 = 10
+num2 = 10
+p add(num1,num2)
+p subtract(num1,num2)
+
+num3 = 30
+num4 = 20
+p add(num3,num4)
+p subtract(num3, num4)
